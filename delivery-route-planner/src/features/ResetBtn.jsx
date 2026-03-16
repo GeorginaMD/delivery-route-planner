a2e68424d163eff3e4b2a65ddeaf9f5716e56d78
Georgina <georginamircheska@gmail.com>
2026-03-16 18:58:43 +0100

    Created new ResetBtn component

---
+++ delivery-route-planner/src/features/ResetBtn.jsx
@@ -0,0 +1,11 @@
+import "./ResetBtn.css"
+
+function ResetBtn() {
+    return (
+        <div className="reset-btn-container">
+            <button className="reset-btn">Reset</button>
+        </div>
+    )
+}
+
+export default ResetBtn;
