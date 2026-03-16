383ab91816bca151c2f6c252007c35d0d63a97f5
Georgina <georginamircheska@gmail.com>
2026-03-16 18:53:57 +0100

    Added components to the Navbar

--- delivery-route-planner/src/sections/Navbar.jsx
+++ delivery-route-planner/src/sections/Navbar.jsx
@@ -1,1 +1,14 @@
 import "./Navbar.css"
+import WorkerBtn from "../components/WorkerBtn";
+import DayTimeHeader from "../components/DayTimeHeader";
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <DayTimeHeader />
+            <WorkerBtn />
+        </div>
+    )
+}
+
+export default Navbar;
