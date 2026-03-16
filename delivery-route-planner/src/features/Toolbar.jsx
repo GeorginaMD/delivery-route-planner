930426298adfa1fe0d5a8c6c82e58d7a4dbc1345
Georgina <georginamircheska@gmail.com>
2026-03-16 18:55:31 +0100

    Added elements to the Toolbar component

--- delivery-route-planner/src/features/Toolbar.jsx
+++ delivery-route-planner/src/features/Toolbar.jsx
@@ -1,1 +1,23 @@
 import "./Toolbar.css"
+
+function Toolbar() {
+    return (
+        <div className="toolbar-panel">
+            <span>🗑️</span>
+            <span>🚨</span>
+            <span>🌞</span>
+            <span>🌚</span>
+            <span>⏰</span>
+            <input type="time" />
+            <span>🟥</span>
+            <span>🟩</span>
+            <span> R </span>
+            <span> D </span>
+            <span>..de..</span>
+            <span>..en..</span>
+            <span> abre -&gt; </span>
+        </div>
+    )
+}
+
+export default Toolbar;
