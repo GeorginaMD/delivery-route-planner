cd5fcad3f576fa99da4ab16dbb1173784131364c
Georgina <georginamircheska@gmail.com>
2026-03-16 18:54:34 +0100

    Added components to the Footer

--- delivery-route-planner/src/sections/Footer.jsx
+++ delivery-route-planner/src/sections/Footer.jsx
@@ -1,1 +1,14 @@
+import ResetBtn from "../features/ResetBtn";
+import SendBtn from "../features/SendBtn";
 import "./Footer.css"
+
+function Footer() {
+    return(
+        <div className="footer">
+            <ResetBtn />
+            <SendBtn />
+        </div>
+    ) 
+};
+
+export default Footer;
