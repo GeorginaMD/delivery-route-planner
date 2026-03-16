23067e7522c8e43848379247b74d9eb31afcecca
Georgina <georginamircheska@gmail.com>
2026-03-16 18:57:16 +0100

    Added elements to the RouteItem component

--- delivery-route-planner/src/components/RouteItem.jsx
+++ delivery-route-planner/src/components/RouteItem.jsx
@@ -1,1 +1,12 @@
 import "./RouteItem.css"
+
+function RouteItem() {
+    return(
+        <li>
+            <span>Clinic A</span>
+            <IconBtn />
+        </li>
+    )
+}
+
+export default RouteItem;
