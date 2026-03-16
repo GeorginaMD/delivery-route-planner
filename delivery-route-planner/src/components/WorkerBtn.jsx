8600017865f31650f7088b6a876833006d9d9640
Georgina <georginamircheska@gmail.com>
2026-03-16 18:52:43 +0100

    Added elements to the Worker component in the Navbar

--- delivery-route-planner/src/components/WorkerBtn.jsx
+++ delivery-route-planner/src/components/WorkerBtn.jsx
@@ -1,1 +1,12 @@
 import "./WorkerBtn.css"
+
+function WorkerBtn() {
+    return (
+        <div className="worker-btn-container">
+            <button className="worker-btn">Worker 1</button>
+            <button className="worker-btn">Worker 2</button>
+        </div>
+    )
+}
+
+export default WorkerBtn;
