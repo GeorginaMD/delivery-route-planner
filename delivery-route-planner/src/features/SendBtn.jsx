a61d544764f0414b7da61e1b025116d93ed7cd11
Georgina <georginamircheska@gmail.com>
2026-03-16 18:59:22 +0100

    Created new SendBtn component

---
+++ delivery-route-planner/src/features/SendBtn.jsx
@@ -0,0 +1,11 @@
+import "./SendBtn.css"
+
+function SendBtn() {
+    return (
+        <div className="send-btn-container">
+            <button className="send-btn">Send to WA</button>
+        </div>
+    )
+}
+
+export default SendBtn;
