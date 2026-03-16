3f3f420682a83b92a1b9d1ce3575119fc4175ce6
Georgina <georginamircheska@gmail.com>
2026-03-16 18:51:03 +0100

    Added elements to the header date component in Navbar

--- delivery-route-planner/src/components/DayTimeHeader.jsx
+++ delivery-route-planner/src/components/DayTimeHeader.jsx
@@ -1,1 +1,12 @@
 import "./DayTimeHeader.css"
+
+function DayTimeHeader() {
+    return (
+        <div className="day-time-header">
+            <span className="date">Lunes 16 Marzo</span>
+            <span> Ultimo enviado: --:-- </span>
+        </div>
+    )
+}
+
+export default DayTimeHeader;
