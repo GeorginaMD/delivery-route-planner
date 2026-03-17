6f3a4a2e2dbc9b5f39b4112c608d75a239c8be6c
Georgina <georginamircheska@gmail.com>
2026-03-17 11:49:37 +0100

    Replaced rendering elements for Components in the main panel

--- delivery-route-planner/src/pages/RoutePanel.jsx
+++ delivery-route-planner/src/pages/RoutePanel.jsx
@@ -1,36 +1,18 @@
 import Toolbar from "../features/Toolbar";
+import LabsList from "../features/LabsList";
 import "./RoutePanel.css"
+import RouteList from "../features/RouteList";
 
 function RoutePanel() {
     return (
-        <div className="route-panel">
+        <div className="main-panel">
 
-            <section className="labs-panel">
-                <header className="labs-header">
-                    <h2>Laboratorios</h2>
-
-                    {/* {isExpanded && (
-                        <button className="add-btn">+</button>
-                    )} */}
-                </header>
-
-                {/* {isExpanded && (
-                    <ul className="labs-list">
-                        <LocationItem />
-                    </ul>
-                )} */}
-
-            </section>
+            <LabsList />
             
-            <div className="labs-list">
-                LABORATORIO
-            </div>
-            <div className="route-list">
-                RUTA
-            </div>
-            <div className="toolbar">
-                <Toolbar />
-            </div>
+            <RouteList />
+
+            <Toolbar />
+            
 
         </div>
     )
