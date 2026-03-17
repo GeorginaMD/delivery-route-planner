d9c0ab01781467c4d9db4e5224ecc169e4c11431
Georgina <georginamircheska@gmail.com>
2026-03-17 11:53:56 +0100

    Added elements to feature components

--- delivery-route-planner/src/features/LabsList.jsx
+++ delivery-route-planner/src/features/LabsList.jsx
@@ -1,1 +1,23 @@
 import "./LabsList.css"
+import LocationItem from "../components/LocationItem";
+
+function LabsList() {
+    return (
+            <section className="labs-panel">
+                <header className="labs-header">
+                    <h2>Laboratorios</h2>
+                </header>
+                <ul className="labs-list">
+                    <LocationItem />
+                    <LocationItem />
+                    <LocationItem />
+                    <LocationItem />
+                    <LocationItem />
+                    <LocationItem />
+                    <LocationItem />
+                </ul>
+            </section>
+    )
+}
+
+export default LabsList;
--- delivery-route-planner/src/features/RouteList.jsx
+++ delivery-route-planner/src/features/RouteList.jsx
@@ -1,1 +1,26 @@
-import "./RouteList.css"
+import "./RouteList.css" 
+import LocationItem from "../components/LocationItem";
+
+function RouteList() {
+    return (
+            <section className="route-panel">
+                <header className="route-header">
+                    <h2>Ruta</h2>
+                </header>
+                <ul className="route-list">
+                    <LocationItem />
+                    <LocationItem />
+                    <LocationItem />
+                    <LocationItem />
+                    <LocationItem />
+                    <LocationItem />
+                    <LocationItem />
+                    <LocationItem />
+                    <LocationItem />
+                    <LocationItem />
+                </ul>
+            </section>
+    )
+}
+
+export default RouteList;
