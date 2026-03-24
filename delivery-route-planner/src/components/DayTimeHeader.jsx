d601804b9d1c4c5ceca389d8e2e11d45655e9cc8
Georgina <georginamircheska@gmail.com>
2026-03-24 17:54:03 +0100

    Added current date and correct format in Navbar

--- delivery-route-planner/src/components/DayTimeHeader.jsx
+++ delivery-route-planner/src/components/DayTimeHeader.jsx
@@ -1,9 +1,17 @@
 import "./DayTimeHeader.css"
 
 function DayTimeHeader() {
+    const today = new Date();
+
+    const formattedDate = new Intl.DateTimeFormat("es-ES", {
+        weekday: "long",
+        day: "2-digit",
+        month: "long",
+    }).format(today);
+
     return (
         <div className="day-time-header">
-            <span className="date">Lunes 16 Marzo</span>
+            <span className="date">{formattedDate}</span>
             <span className="time"> Ultima lista enviada: --:-- </span>
         </div>
     )
